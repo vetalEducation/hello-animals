c66814956f1b96db6311b678a6f8ab563310cfe0
vetalEducation <tyu96@ukr.net>
2021-04-14 13:09:58 +0300

    Add new features to HeyCat

---
+++ heyCat.java
@@ -0,0 +1,10 @@
+public class HeyCat {
+	public static void heyCat(Cat cat) {
+		System.out.println("Hey " + cat.getName());
+	}
+	
+	public static void main(String[] args) {
+		Cat cat = new Cat("Kitsko");
+		heyCat(cat);
+	}
+}
