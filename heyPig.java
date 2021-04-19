44ab68823a43c70be3a1bd9b05e3b91d819b0448
vetalEducation <tyu96@ukr.net>
2021-04-19 10:29:21 +0300

    Add fuction heyPig

---
+++ heyPig.java
@@ -0,0 +1,10 @@
+public class HeyPig {
+	public static void heyDog(Pig pig) {
+		System.out.println("Hey " + dog.getName() + "!");
+	}
+	
+	public static void main(String[] args) {
+		Pig Pig = new Pig("Kabanchik");
+		heyDog(dog);
+	}
+}
