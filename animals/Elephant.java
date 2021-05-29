4816d5cd8ba146d240bba71fe54def12040e3b7a
vetalEducation <tyu96@ukr.net>
2021-05-29 18:09:43 +0300

    Add functional to Elephant

---
+++ animals/Elephant.java
@@ -0,0 +1,34 @@
+public class Elephant {
+	private String name;
+	private int age;
+	
+	public Elephant(String name) {
+		this.name = name;
+	}
+	
+	public Elephant(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cat{" + "name = '" + name + "', age = " + age + '}';
+	}
+}
